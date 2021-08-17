0f9c082c35d0aca218abde4617154d1a44461ef3
Юрий <yurii.shilkin@gmail.com>
2021-08-17 14:07:26 +0300

    шаблон

---
+++ Clone1C/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Clone1C
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+
+            InitializeComponent();
+
+            using (UserContext db = new UserContext())
+            {
+                            User user1 = new User { Name = "Tom", Age = 33 };
+                User user2 = new User { Name = "Sam", Age = 26 };
+                db.Users.Add(user1);
+                db.Users.Add(user2);
+                db.SaveChanges();
+                Console.WriteLine("Объекты успешно сохранены");
+            }
+
+        }
+    }
+}
---
+++ Clone1C/UserContext .cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace Clone1C
+{
+    class UserContext : DbContext
+    {
+        public UserContext()
+            : base("DbConnection")
+        { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
